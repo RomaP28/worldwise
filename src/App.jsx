6d6b5ba5f040782f15e551a5d4bb89a825ceda8d
RomaP28 <posokhovrom@gmail.com>
2023-10-28 18:14:36 +0300

    Added url state

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import AppLayout from './pages/AppLayout';
 import CityList from "./components/CityList";
 import CountryList from "./components/CountryList";
 import { useEffect, useState } from 'react';
+import City from "./components/City";
 
 const BASE_URL = 'http://localhost:8000';
 
@@ -34,17 +35,17 @@ function App() {
   return (
     <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Homepage />} />
-          <Route path="app" element={<AppLayout />}>
-            <Route index element={<CityList cities={cities} isLoading={isLoading} />} />
-            <Route path="cities" element={<CityList isLoading={isLoading} />} />
-            <Route path="countries" element={<CountryList cities={cities} isLoading={isLoading} />} />
-            <Route path="form" element={<p>Form</p>} />
-          </Route>
-          <Route path="/login" element={<Login />} />
-          <Route path="product" element={<Product />} />
-          <Route path="pricing" element={<Pricing />} />
-          <Route path="*" element={<PageNotFound />} />
+			<Route path="/" element={<Homepage />} />
+			<Route path="product" element={<Product />} />
+			<Route path="pricing" element={<Pricing />} />
+			<Route path="/login" element={<Login />} />
+			<Route path="app" element={<AppLayout />}>
+				<Route index element={<CityList cities={cities} isLoading={isLoading} />} />
+				<Route path="cities" element={<CityList cities={cities} isLoading={isLoading} />} />
+				<Route path='cities/:id' element={<City />} />
+				<Route path="countries" element={<CountryList cities={cities} isLoading={isLoading} />} />
+				<Route path="form" element={<p>Form</p>} /></Route>
+			<Route path="*" element={<PageNotFound />} />
         </Routes>
     </BrowserRouter>
   )
